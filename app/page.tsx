23216e7cae4399a4578077af4dbcbd5d323e27b3
Yuta Okafuji <ofuji.works@gmail.com>
2024-04-29 22:53:02 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { type NextPage } from "next";
 
 import Encorder from "../feature/encoder";
+ 
+export const runtime = "edge";
 
 const Page: NextPage = () => {
   return (
--- hooks/useEncode.ts
+++ hooks/useEncode.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState, useRef } from "react";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { toBlobURL } from "@ffmpeg/util";
